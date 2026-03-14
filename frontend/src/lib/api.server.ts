d381aa9a35ce817e0a0723d092c569c7e6f3a9b1
CuteFox <alicepolishchuk1@gmail.com>
2026-03-14 17:01:45 +0200

    fix: build fixes

--- frontend/src/lib/api.server.ts
+++ frontend/src/lib/api.server.ts
@@ -1,10 +1,6 @@
 import "server-only";
 import axios from "axios";
 
-if (!process.env.BACKEND_URL) {
-  throw new Error("BACKEND_URL is not set");
-}
-
 export const serverApi = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: process.env.BACKEND_URL || "http://localhost:5000",
 });
